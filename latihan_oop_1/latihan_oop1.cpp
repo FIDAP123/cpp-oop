379d81d7cf0e44760d430f887153bec425855a4a
FIDAP123 <fitradanaputra@gmail.com>
2022-06-02 19:18:51 +0700

    edit

---
+++ latihan_oop_1/latihan_oop1.cpp
@@ -0,0 +1,7 @@
+/*
+    Latihan OOP 1 : Pendahuluan
+    1. Alasan kenapa harus belajar OOP :
+        a.  karena untuk dasar kita baru belajar pemrograman dengan paradigma 
+            prosedural
+        b.  
+*/
