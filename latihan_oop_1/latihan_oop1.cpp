033ba5e0aaaa40494c239975010bff5da7c5cfcf
FIDAP123 <fitradanaputra@gmail.com>
2022-06-05 17:45:21 +0700

    edit

--- latihan_oop_1/latihan_oop1.cpp
+++ latihan_oop_1/latihan_oop1.cpp
@@ -3,5 +3,33 @@
     1. Alasan kenapa harus belajar OOP :
         a.  karena untuk dasar kita baru belajar pemrograman dengan paradigma 
             prosedural
-        b.  
+        b.  karena dengan OOP kita bisa membuat aplikasi yang kompleks dengan memanfaatkan konsep class dan object 
+            didalamnya
+        c.  Didalam class harus ditambahkan parameter public, private, protected
+        d.  Didalam class bisa terdapat data member dan data function / method atau fungsi
 */
+
+#include<iostream>
+
+class Orang
+{
+public:
+    std::string nama;
+private:
+    std::string alamat;
+protected:
+    std::string gender;
+};
+
+int main(int argc, char const *argv[])
+{
+    Orang orang1;
+
+    orang1.nama = "wawan";
+    // orang1.alamat = "jepara";
+    // orang1.gender = "cwk";
+
+    std::cout << "selesai" << std::endl;
+    
+    return 0;
+}
