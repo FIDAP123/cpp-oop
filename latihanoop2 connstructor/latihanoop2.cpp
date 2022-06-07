8995df2d7ecda2273c78554db6730059b3d95246
FIDAP123 <fitradanaputra@gmail.com>
2022-06-07 15:58:54 +0700

    edit

--- latihanoop2 connstructor/latihanoop2.cpp
+++ latihanoop2 connstructor/latihanoop2.cpp
@@ -5,8 +5,21 @@
 
 #include<iostream>
 
+class Manusia
+{
+public:
+    Manusia(/* args */)
+    {
+        std::cout << "halo" << std::endl;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    std::cout << "halo" << std::endl;
+    Manusia manusia1 = Manusia();// dengan adanya constructor kita bisa lebih mudah memanggil class dengan meng-inisialisasikan obeject jika constructor mempunyai parameter atau kita bisa hanya memanggil class tanpa meng-inisialisasikannya
+    Manusia manusia2;//ini adalah contoh pemanggilan class dengan constructor yg tidak mempunyai parameter dengan hanya membuat objeknya saja
+    
+    std::cout << std::endl;
+    std::cout << "selesai" << std::endl;
     return 0;
 }
